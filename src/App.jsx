aa4c85e9c6eaa09a6bac9f8819e973dff33c8105
RamiroVidal26 <110636851+RamiroVidal26@users.noreply.github.com>
2023-11-09 00:11:33 -0300

    details

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { IndexPage } from "./pages/IndexPage";
 import { LoginPage } from "./pages/LoginPage";
+import { Layout } from "./Layout";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" index element={<IndexPage />}></Route>
-      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
     </Routes>
   );
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+
 export const LoginPage = () => {
-  return "login must be here";
+  return (
+    <div className=" pt-6 flex flex-col grow justify-around  ">
+      <div className="mb-64">
+        <h1 className=" text-4xl mb-4  text-center">login</h1>
+        <form action="" className=" max-w-md mx-auto">
+          <input type="email" placeholder="your@email.com" />
+          <input type="password" placeholder="you Password" />
+          <button className="primary">Login</button>
+          <div className="py-2 text-center">
+            Don't have an account yet? <Link to={"/register"}>Register</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
