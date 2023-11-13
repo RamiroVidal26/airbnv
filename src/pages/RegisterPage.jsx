c0b2c808797d0f1a342d7a5e5de20df0e5edae89
RamiroVidal26 <110636851+RamiroVidal26@users.noreply.github.com>
2023-11-13 13:10:19 -0300

    cosas

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,15 +1,62 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const RegisterPage = () => {
+  const [formState, setFormState] = useState({
+    userName: "Diego Forlán",
+    email: "DF10@gmail.com",
+    password: "12345",
+  });
+
+  const { userName, email, password } = formState;
+
+  const onInputValue = (event) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(event.target);
+  };
+
+  const registerUser = async (ev) => {
+    ev.preventDefault();
+    const response = axios.post("/register", {
+      userName,
+      email,
+      password,
+    });
+    console.log("respuesta servidor", response);
+  };
+
   return (
     <div className=" pt-6 flex flex-col grow justify-around  ">
       <div className="mb-64">
         <h1 className=" text-4xl mb-4  text-center">login</h1>
-        <form action="" className=" max-w-md mx-auto">
-          <input type="name" placeholder="Diego Forlán" />
-          <input type="email" placeholder="DF10@gmail.com.com" />
-          <input type="password" placeholder="**********" />
-          <button className="primary">Login</button>
+        <form action="" className=" max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            name="userName"
+            placeholder="Diego Forlán"
+            value={userName}
+            onChange={onInputValue}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="DF10@gmail.com.com"
+            value={email}
+            onChange={onInputValue}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="**********"
+            value={password}
+            onChange={onInputValue}
+          />
+          <button className="primary">Register</button>
           <div className="py-2 text-center">
             Already a member?{" "}
             <Link className="underline" to={"/login"}>
