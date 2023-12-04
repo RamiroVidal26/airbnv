e48b59c632ecfacaff7ea86b5ef1426fed898ef3
RamiroVidal26 <110636851+RamiroVidal26@users.noreply.github.com>
2023-12-04 15:45:08 -0300

    new pages

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -5,11 +5,37 @@ export const PlacesPage = () => {
   const { action } = useParams();
   const textAreaRef = useRef(null);
   const [val, setVal] = useState("");
+  const [val2, setVal2] = useState("");
+  const [selectedCheckIn, setSelectedCheckin] = useState("");
+  const [selectedCheckOut, setSelectedCheckOut] = useState("");
+
+  const hours = [
+    "8:00 AM",
+    "9:00 AM",
+    "10:00 AM",
+    "11:00 AM",
+    "12:00 PM",
+    "1:00 PM",
+    "2:00 PM",
+    "3:00 PM",
+    "4:00 PM",
+    "5:00 PM",
+    "6:00 PM",
+    "7:00 PM",
+  ];
 
   const handleTextareaChange = (e) => {
     setVal(e.target.value);
   };
-
+  const handleTextareaChange2 = (e) => {
+    setVal2(e.target.value);
+  };
+  const handleCheckInChange = (e) => {
+    setSelectedCheckin(e.target.value);
+  };
+  const handleCheckOutChange = (e) => {
+    setSelectedCheckOut(e.target.value);
+  };
   useEffect(() => {
     if (textAreaRef.current) {
       textAreaRef.current.style.height = "auto";
@@ -58,14 +84,12 @@ export const PlacesPage = () => {
             <h2 className="text-xl font-medium mb-1 mt-3">
               Título para la publicación
             </h2>
-
             <input
               type="text"
               placeholder=" 'Apartamento a metros de la playa' "
             />
             <h2 className="text-xl font-medium mb-1 mt-3">Dirección</h2>
             <input type="text" placeholder=" 'Calle siempreviva 742' " />
-
             <h2 className="text-xl font-medium mb-1 mt-3">Fotos</h2>
             <label
               htmlFor="fileInput"
@@ -111,7 +135,7 @@ export const PlacesPage = () => {
             </div>
             <h2 className="text-xl font-medium mb-1 mt-3">Servicios</h2>
             <div className="grid gap-2 items-center  grid-cols-2 md:grid-cols-4 lg:grid-cols-6 ">
-              <label className="border p-4 shadow-md  gap-2 flex">
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
                 <input type="checkbox" />
                 <span className="flex gap-1 items-center ">
                   <svg
@@ -131,7 +155,7 @@ export const PlacesPage = () => {
                   Wifi
                 </span>
               </label>
-              <label className="border p-4 shadow-md  gap-2 flex">
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
                 <input type="checkbox" />
                 <span className="flex gap-1 items-center ">
                   <svg
@@ -141,10 +165,24 @@ export const PlacesPage = () => {
                   >
                     <path d="M96 24c0-13.3 10.7-24 24-24h80c13.3 0 24 10.7 24 24V48h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H88C74.7 96 64 85.3 64 72s10.7-24 24-24h8V24zM0 256c0-70.7 57.3-128 128-128H256c70.7 0 128 57.3 128 128V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256zm256 0v96c0 17.7 14.3 32 32 32s32-14.3 32-32V256c0-17.7-14.3-32-32-32s-32 14.3-32 32z" />
                   </svg>
-                  Lavadora
+                  Lavarropa
                 </span>
               </label>
-              <label className="border p-4 shadow-md  gap-2 flex">
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
+                <input type="checkbox" />
+                <span className="flex gap-1 items-center ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="16"
+                    width="16"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM323.8 202.5c-4.5-6.6-11.9-10.5-19.8-10.5s-15.4 3.9-19.8 10.5l-87 127.6L170.7 297c-4.6-5.7-11.5-9-18.7-9s-14.2 3.3-18.7 9l-64 80c-5.8 7.2-6.9 17.1-2.9 25.4s12.4 13.6 21.6 13.6h96 32H424c8.9 0 17.1-4.9 21.2-12.8s3.6-17.4-1.4-24.7l-120-176zM112 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z" />
+                  </svg>{" "}
+                  Paisajes
+                </span>
+              </label>
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
                 <input type="checkbox" />
                 <span className="flex gap-1 items-center ">
                   <svg
@@ -157,7 +195,7 @@ export const PlacesPage = () => {
                   A/C
                 </span>
               </label>
-              <label className="border p-4 shadow-md  gap-2 flex">
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
                 <input type="checkbox" />
                 <span className="flex gap-1 items-center ">
                   <svg
@@ -170,19 +208,141 @@ export const PlacesPage = () => {
                   Cocina
                 </span>
               </label>
-              <label className="border p-4 shadow-md  gap-2 flex">
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
                 <input type="checkbox" />
-                <span className="flex gap-1 items-center ">Secadora</span>
+                <span className="flex gap-1 items-center ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="16"
+                    width="20"
+                    viewBox="0 0 640 512"
+                  >
+                    <path d="M96 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32V224v64V448c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32V384H64c-17.7 0-32-14.3-32-32V288c-17.7 0-32-14.3-32-32s14.3-32 32-32V160c0-17.7 14.3-32 32-32H96V64zm448 0v64h32c17.7 0 32 14.3 32 32v64c17.7 0 32 14.3 32 32s-14.3 32-32 32v64c0 17.7-14.3 32-32 32H544v64c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32V288 224 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32zM416 224v64H224V224H416z" />
+                  </svg>
+                  Gimnasio
+                </span>
               </label>
-              <label className="border p-4 shadow-md  gap-2 flex">
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
                 <input type="checkbox" />
-                <span className="flex gap-1 items-center ">Calefacción</span>
+                <span className="flex gap-1 items-center ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
+                    />
+                  </svg>
+                  Televisor
+                </span>
               </label>
-              <label className="border p-4 shadow-md  gap-2 flex">
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
                 <input type="checkbox" />
-                <span className="flex gap-1 items-center "></span>
+                <span className="flex gap-1 items-center ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="16"
+                    width="18"
+                    viewBox="0 0 576 512"
+                  >
+                    <path d="M309.6 158.5L332.7 19.8C334.6 8.4 344.5 0 356.1 0c7.5 0 14.5 3.5 19 9.5L392 32h52.1c12.7 0 24.9 5.1 33.9 14.1L496 64h56c13.3 0 24 10.7 24 24v24c0 44.2-35.8 80-80 80H464 448 426.7l-5.1 30.5-112-64zM416 256.1L416 480c0 17.7-14.3 32-32 32H352c-17.7 0-32-14.3-32-32V364.8c-24 12.3-51.2 19.2-80 19.2s-56-6.9-80-19.2V480c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V249.8c-28.8-10.9-51.4-35.3-59.2-66.5L1 167.8c-4.3-17.1 6.1-34.5 23.3-38.8s34.5 6.1 38.8 23.3l3.9 15.5C70.5 182 83.3 192 98 192h30 16H303.8L416 256.1zM464 80a16 16 0 1 0 -32 0 16 16 0 1 0 32 0z" />
+                  </svg>
+                  Mascotas
+                </span>
+              </label>
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
+                <input type="checkbox" />
+                <span className="flex gap-1 items-center ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="16"
+                    width="20"
+                    viewBox="0 0 640 512"
+                  >
+                    <path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z" />
+                  </svg>{" "}
+                  Visitas{" "}
+                </span>
+              </label>
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
+                <input type="checkbox" />
+                <span className="flex gap-1 items-center ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="16"
+                    width="18"
+                    viewBox="0 0 576 512"
+                  >
+                    <path d="M309.5 178.4L447.9 297.1c-1.6 .9-3.2 2-4.8 3c-18 12.4-40.1 20.3-59.2 20.3c-19.6 0-40.8-7.7-59.2-20.3c-22.1-15.5-51.6-15.5-73.7 0c-17.1 11.8-38 20.3-59.2 20.3c-10.1 0-21.1-2.2-31.9-6.2C163.1 193.2 262.2 96 384 96h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H384c-26.9 0-52.3 6.6-74.5 18.4zM160 160A64 64 0 1 1 32 160a64 64 0 1 1 128 0zM306.5 325.9C329 341.4 356.5 352 384 352c26.9 0 55.4-10.8 77.4-26.1l0 0c11.9-8.5 28.1-7.8 39.2 1.7c14.4 11.9 32.5 21 50.6 25.2c17.2 4 27.9 21.2 23.9 38.4s-21.2 27.9-38.4 23.9c-24.5-5.7-44.9-16.5-58.2-25C449.5 405.7 417 416 384 416c-31.9 0-60.6-9.9-80.4-18.9c-5.8-2.7-11.1-5.3-15.6-7.7c-4.5 2.4-9.7 5.1-15.6 7.7c-19.8 9-48.5 18.9-80.4 18.9c-33 0-65.5-10.3-94.5-25.8c-13.4 8.4-33.7 19.3-58.2 25c-17.2 4-34.4-6.7-38.4-23.9s6.7-34.4 23.9-38.4c18.1-4.2 36.2-13.3 50.6-25.2c11.1-9.4 27.3-10.1 39.2-1.7l0 0C136.7 341.2 165.1 352 192 352c27.5 0 55-10.6 77.5-26.1c11.1-7.9 25.9-7.9 37 0z" />
+                  </svg>
+                  Piscina
+                </span>
+              </label>
+              <label className="border p-4 shadow-md  gap-2 flex cursor-pointer">
+                <input type="checkbox" />
+                <span className="flex gap-1 items-center ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="20"
+                    width="20"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M99.5 144.8L178.7 224l96 96 92.5 92.5C335.9 434.9 297.5 448 256 448C150 448 64 362 64 256c0-41.5 13.1-79.9 35.5-111.2zM333.3 288l-32-32H384v32H333.3zm32 32H400c8.8 0 16-7.2 16-16V240c0-8.8-7.2-16-16-16H269.3L144.8 99.5C176.1 77.1 214.5 64 256 64c106 0 192 86 192 192c0 41.5-13.1 79.9-35.5 111.2L365.3 320zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM272 96c-8.8 0-16 7.2-16 16c0 26.5 21.5 48 48 48h32c8.8 0 16 7.2 16 16s7.2 16 16 16s16-7.2 16-16c0-26.5-21.5-48-48-48H304c-8.8 0-16-7.2-16-16s-7.2-16-16-16zM229.5 320l-96-96H112c-8.8 0-16 7.2-16 16v64c0 8.8 7.2 16 16 16H229.5z" />
+                  </svg>
+                  No Fumar
+                </span>
               </label>
             </div>
+            <h2 className="text-xl font-medium mb-1 mt-3">Info Extra</h2>
+            <div className="relative w-full min-w-[200px] mb-1 mt-3">
+              <textarea
+                className="peer h-auto overflow-hidden w-full resize-none rounded-[7px] border border-blue-gray-200  bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-primary focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50 "
+                value={val2}
+                onChange={handleTextareaChange2}
+                ref={textAreaRef}
+                rows={5}
+              ></textarea>
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-primary peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-primary peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Información extra
+              </label>
+            </div>
+            <h2 className="text-xl font-medium mb-1 mt-3">
+              Check-in y Check-out
+            </h2>
+            <select
+              id="horarios"
+              value={selectedCheckIn}
+              onChange={handleCheckInChange}
+            >
+              <option value="" disabled>
+                Selecciona un horario
+              </option>
+              {hours.map((hours) => (
+                <option key={hours} value={hours}>
+                  {hours}
+                </option>
+              ))}
+            </select>
+            <select
+              id="horarios"
+              value={selectedCheckOut}
+              onChange={handleCheckOutChange}
+            >
+              <option value="" disabled>
+                Selecciona un horario
+              </option>
+              {hours.map((hours) => (
+                <option key={hours} value={hours}>
+                  {hours}
+                </option>
+              ))}
+            </select>
           </form>
         </div>
       )}
